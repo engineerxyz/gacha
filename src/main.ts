37af6f1801779d494a04b9084ef5d792e68dd956
engineerxyz <engineerxyz00@gmail.com>
2026-02-13 00:16:25 +0900

    chore: expose game for debugging

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ import { startPhaserGame } from './phaserGame'
 const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = `<div id="game"></div>`
 
-startPhaserGame(document.querySelector<HTMLDivElement>('#game')!)
+const game = startPhaserGame(document.querySelector<HTMLDivElement>('#game')!)
+// debug hook
+;(window as any).__gacha_game = game
